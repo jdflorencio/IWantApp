2b40248f318812cf82b2f8e2a9b6322c8a546525
João Diego Florencio <jdflorencio@gmail.com>
2023-11-28 12:52:13 -0300

    feat(category): validando

--- src/Domain/Products/Category.cs
+++ src/Domain/Products/Category.cs
@@ -1,8 +1,21 @@
-﻿namespace IWantApp.Domain.Products;
+﻿using Flunt.Validations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace IWantApp.Domain.Products;
 
 public class Category: Entity
-{
+{    
     public string Name { get; set; }
-    public bool Active { get; set; } = true;
+ 
+    public bool Active { get; set; }
+    public Category(string name) {
+        var contract = new Contract<Category>()
+            .IsNotNullOrEmpty(name, "Name ´e obrigatorio");
+            AddNotifications(contract);
+
+        Name = name;
+        Active = true;
+        
+    }
 
 }
