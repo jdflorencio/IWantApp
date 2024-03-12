dc8569569992c54cda3164db1194e7498f974501
João Diego Florencio <jdflorencio@gmail.com>
2024-03-11 22:55:46 -0300

    Configurando autenticação e autorização

--- src/Program.cs
+++ src/Program.cs
@@ -2,43 +2,74 @@ using IWantApp.EndPoints.Categories;
 using IWantApp.EndPoints.Employees;
 using IWantApp.EndPoints.Security;
 using IWantApp.Infra.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using static System.Collections.Immutable.ImmutableArray<T>;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionString:IWantDb"]);
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => {
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionString:IWantDb"]);
+        builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+        {
 
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequiredLength = 3;
-}).AddEntityFrameworkStores<ApplicationDbContext>();
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequiredLength = 3;
+        }).AddEntityFrameworkStores<ApplicationDbContext>();
 
-builder.Services.AddAuthorization();
+        builder.Services.AddAuthorization();
+        builder.Services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateActor = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidAudience = builder.Configuration["JwtBearerTokenSettings:Audience"],
+                ValidIssuer = builder.Configuration["JwtBearerTokenSettings:Issuer"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SecretKey"]))
+            };
+        });
+
+
 
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+        var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.MapMethods(CategoryPost.Template, CategoryPost.Methods, CategoryPost.Handle);
+        app.MapMethods(CategoryGetAll.Template, CategoryGetAll.Methods, CategoryGetAll.Handle);
+        app.MapMethods(CategoryPut.Template, CategoryPut.Methods, CategoryPut.Handle);
+        app.MapMethods(EmployeePost.Template, EmployeePost.Methods, EmployeePost.Handle);
+        app.MapMethods(EmployeesGetAll.Template, EmployeesGetAll.Methods, EmployeesGetAll.Handle);
+        app.MapMethods(TokenPost.Template, TokenPost.Methods, TokenPost.Handle);
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.MapMethods(CategoryPost.Template, CategoryPost.Methods, CategoryPost.Handle);
-app.MapMethods(CategoryGetAll.Template, CategoryGetAll.Methods, CategoryGetAll.Handle);
-app.MapMethods(CategoryPut.Template, CategoryPut.Methods, CategoryPut.Handle);
-app.MapMethods(EmployeePost.Template, EmployeePost.Methods, EmployeePost.Handle);
-app.MapMethods(EmployeesGetAll.Template, EmployeesGetAll.Methods, EmployeesGetAll.Handle);
-app.MapMethods(TokenPost.Template, TokenPost.Methods, TokenPost.Handle);
-
-app.Run();
